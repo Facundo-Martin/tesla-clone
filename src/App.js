f8e333f23108aac3f0a9d8a447ead58bf193d96e
Facundo Martin <facundomartin98@gmail.com>
2022-04-20 09:36:47 -0400

    added Section component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Header from "./comopnents/Header";
+import Home from "./comopnents/Home";
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <Home />
     </div>
   );
 }
